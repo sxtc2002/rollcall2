65404ed1ab96a08e635d2acc26cccaa14e7b6359
sxtc2002 <2499948209@qq.com>
2023-05-23 14:07:15 +0800

    添加Controller

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,7 +1,36 @@
-CREATE TABLE `Counters` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `count` int(11) NOT NULL DEFAULT '1',
-  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
+CREATE TABLE if not exists Teacher
+(
+    Teacher_Id CHAR (20) PRIMARY KEY,
+    Teacher_Name CHAR (20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE if not exists Student
+(
+    Student_Id CHAR (20) PRIMARY KEY,
+    Student_Name CHAR (20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE if not exists Course
+(
+    Course_Id CHAR(20) PRIMARY KEY,
+    Course_Name CHAR (20) NOT NULL,
+    Teacher_Id CHAR (20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE if not exists Schedules
+(
+    Course_Id CHAR(20) NOT NULL,
+    Course_Date CHAR (20) NOT NULL,
+    PRIMARY KEY(Course_Id, Course_Date)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE if not exists Selection
+(
+    Course_Id CHAR(20) NOT NULL,
+    Student_Id CHAR (20) NOT NULL,
+    PRIMARY KEY(Course_Id, Student_Id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE if not exists State
+(
+    Student_Id CHAR (20) NOT NULL,
+    Course_Id CHAR(20) NOT NULL,
+    Course_Date CHAR (10) NOT NULL,
+    Student_State CHAR (20) CHECK (Student_State='present' OR Student_State='absent' OR Student_State='leave'),
+    PRIMARY KEY(Student_Id, Course_Id, Course_Date)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
