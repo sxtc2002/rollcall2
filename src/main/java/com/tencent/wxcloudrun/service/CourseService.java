3ed6035ee932ecc621c8d3264291a322d822fe10
sxtc2002 <2499948209@qq.com>
2023-05-23 14:23:09 +0800

    修复bug

--- src/main/java/com/tencent/wxcloudrun/service/CourseService.java
+++ src/main/java/com/tencent/wxcloudrun/service/CourseService.java
@@ -1,9 +1,11 @@
 package com.tencent.wxcloudrun.service;
 
 import com.tencent.wxcloudrun.model.Course;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service
 public interface CourseService {
     void insert(String Course_Id, String Course_Name, String Teacher_Id);
     void delete(String Course_Id);
